7059f65248249bdab019f0c63be6ce0c79ca3dd8
JaxsonMitchell <MitchJ62@my.erau.edu>
2024-02-19 21:17:55 -0700

    Testing relative import

--- dpt/__init__.py
+++ dpt/__init__.py
@@ -1,2 +1,2 @@
-from dpt.transform import DPT, VVT, VVT2, STPT, ISTPT
-from dpt.functions import c_n, s_n, nGauss, Signal, VoxelGrid, PixelGrid
+from .transform import DPT, VVT, VVT2, STPT, ISTPT
+from .functions import c_n, s_n, nGauss, Signal, VoxelGrid, PixelGrid
