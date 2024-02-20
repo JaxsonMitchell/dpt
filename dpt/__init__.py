cbc7eb5742fa4bf6eebb6199e5b0bc5cb88bfb0f
JaxsonMitchell <MitchJ62@my.erau.edu>
2024-02-19 20:57:05 -0700

    Add functions.py in dpt folder

--- dpt/__init__.py
+++ dpt/__init__.py
@@ -1,4 +1,2 @@
 from dpt.transform import DPT, VVT, VVT2, STPT, ISTPT
-from comp.dpfunc import c_n, s_n
-from comp.sgn import Signal
-from comp.pixel import VoxelGrid, PixelGrid
+from dpt.functions import c_n, s_n, nGauss, Signal, VoxelGrid, PixelGrid
---
+++ dpt/functions.py
@@ -0,0 +1,3 @@
+from comp.dpfunc import c_n, s_n, nGauss
+from comp.sgn import Signal
+from comp.pixel import VoxelGrid, PixelGrid
