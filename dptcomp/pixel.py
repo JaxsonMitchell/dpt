619ef9d75b85531afc6feb9454560ab2858078b5
JaxsonMitchell <MitchJ62@my.erau.edu>
2024-07-06 19:59:02 -0700

    fix again

--- dptcomp/pixel.py
+++ dptcomp/pixel.py
@@ -411,7 +411,7 @@ def setTimeFreqAxes(
     ax.set_yticks(range(0, len(XY_data[1]), XY_skips[1]))
     
     ax.set_xticklabels(np.round(XY_data[0][::XY_skips[0]], 4), fontsize=XY_fontsize[0], rotation=90)
-    ax.set_yticklabels(np.angleround(XY_data[1][::XY_skips[1]], 4), fontsize=XY_fontsize[1])
+    ax.set_yticklabels(np.round(XY_data[1][::XY_skips[1]], 4), fontsize=XY_fontsize[1])
 
     labelTimeFreqAxes(ax, XY_fontsize_labels, xlabel, ylabel, title)
 
